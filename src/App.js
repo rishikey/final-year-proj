11c7d7d79e52777641a27fa6381338b451a8efc3
rishikey <yrishikey39@gmail.com>
2023-05-17 17:43:01 +0530

    commit2 working

--- src/App.js
+++ src/App.js
@@ -3,17 +3,45 @@
   ॥ श्री गणेशाय नमः ॥
 it's the WILL not the skill
  */
-
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 import Navbar from'./components/navbar.js'
-
+import Lab from './components/Labsection/Lab.js'
 import Content from './components/Content'
 function App() {
   return (  
 <>
-    <Navbar />
+<div className="container">
+<Router>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="/">ImAble</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ">
+          <li className="nav-item active">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav-item active">
+            <Link to="/Labs">Labs</Link>
+          </li>
+          <li className="nav-item  ml-auto">
+            <Link to="/">join</Link>
+          </li>
 
-    <Content/>
-    
+        </ul>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Content />} />
+        <Route path="/Labs" element={<Lab />} />
+        
+      </Routes>
+    </Router>
+
+
+</div>
 </>
   );
 }
