496a01da3766b37221f6610876cf0bf5ee07fb38
rishikey <yrishikey39@gmail.com>
2023-05-17 18:17:52 +0530

    navbar adjusted

--- src/App.js
+++ src/App.js
@@ -10,20 +10,20 @@ import Content from './components/Content'
 function App() {
   return (  
 <>
-<div className="container">
-<Router>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+<div >
+<Router style={{"margin":"0px","border":"0px","padding":"0px" }}>
+      <nav className="navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar" style={{"margin":"0px","border":"0px","background":"purple","padding":"0px","overflow-x":"hidden"}}>
       <a className="navbar-brand" href="/">ImAble</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
-  </button>
+  </button> */}
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ">
           <li className="nav-item active">
             <Link to="/">Home</Link>
           </li>
           <li className="nav-item active">
-            <Link to="/Labs">Labs</Link>
+            <Link to="/Labs">|Labs</Link>
           </li>
           <li className="nav-item  ml-auto">
             <Link to="/">join</Link>
